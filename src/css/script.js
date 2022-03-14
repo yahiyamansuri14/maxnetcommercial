727e2bfee70594a8fc86795a01c0735af6c7b0d9
yahiya_mansuri <yahiyamansuri14@gmail.com>
2022-03-14 23:51:05 +0530

    Create script.js

---
+++ src/css/script.js
@@ -0,0 +1,13 @@
+ function openForm(event, requestName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("request-button");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(requestName).style.display = "block";
+            event.currentTarget.className += " active";
+        }
